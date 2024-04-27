0eb50a13c5b9cc502ff061886f7a96334f9f953b
Ilia Afanasev <afanasev.ilya.a@yandex.ru>
2024-04-27 23:46:54 +0300

    new: userController

--- src/common/route.interface.ts
+++ src/common/route.interface.ts
@@ -2,6 +2,6 @@ import { NextFunction, Response, Request, Router } from "express";
 
 export interface IControllerRoute {
 	path: string;
-	func(): (req: Request, res: Response, next: NextFunction) => void;
+	func: (req: Request, res: Response, next: NextFunction) => void;
 	method: keyof Pick<Router, "get" | "post" | "put" | "delete" | "patch">;
 }
