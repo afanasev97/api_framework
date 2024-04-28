29fb8d0d5774949321b5d80c859f49d53cbc3023
Ilia Afanasev <afanasev.ilya.a@yandex.ru>
2024-04-28 23:09:30 +0300

    new: exeption filter

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { BaseController } from "../common/base.controller";
 import { LoggerService } from "../logger/logger.service";
+import { HTTPError } from "../errors/http-error.class";
 
 export class UserController extends BaseController {
 	constructor(
@@ -8,13 +9,14 @@ export class UserController extends BaseController {
 	) {
 		super(logger);
 		this.bindRoutes([
-			{ path: "/login", method: "get", func: this.register },
+			{ path: "/login", method: "get", func: this.login },
 			{ path: "/register", method: "post", func: this.register }
 		])
 	}
 
 	login(req: Request, res: Response, next: NextFunction) {
-		this.ok(res, "login");
+		next(new HTTPError(401, "Authorize error"));
+		// this.ok(res, "login");
 	}
 	
 	register(req: Request, res: Response, next: NextFunction) {
